f28d9e6027699af97440fbbc948c038c82aa48ae
Никита Овечкин <nikita.ovechkin@gazprombank.ru>
2024-09-23 15:23:14 +0200

    feat: добавлен виджет калорий

--- src/pages/statistics/StatisticsPage.tsx
+++ src/pages/statistics/StatisticsPage.tsx
@@ -1,5 +1,13 @@
+import RecommendedCaloriesWidget from "@widgets/recommended-calories-widget";
+
+import s from "./StatisticsPage.module.scss";
+
 const StatisticsPage = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className={s.root}>
+      <RecommendedCaloriesWidget />
+    </div>
+  );
 };
 
 export default StatisticsPage;
