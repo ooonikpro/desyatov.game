90a043b75530da94d35fc1d385700c3ccfe4fc2a
ninja095 <ninja095@gmail.com>
2024-10-02 12:38:14 +0200

    issue-4: добавлен CORS

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,7 +1,14 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 import { Routes } from "./routes/example.route";
 
 const server = fastify( { logger: true } );
+
+server.register( cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+} );
 
 server.register( Routes );
 
