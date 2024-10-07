d46147867744cda9a2086dbcc37c4764d5638642
Musajiji <dzhal@bk.ru>
2024-10-07 13:16:31 +0300

    поправил конфиг

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,33 +1,52 @@
+import autoprefixer from "autoprefixer";
 import * as path from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
 
+type ViteConfig = {
+  mode: string;
+  command: string;
+};
+
 const APP_BASE_URL = process.env.APP_BASE_URL ?? "";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: APP_BASE_URL,
-  build: {
-    outDir: path.resolve(__dirname, "../../dist"),
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@widgets": path.resolve(__dirname, "./src/widgets"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@entities": path.resolve(__dirname, "./src/entities"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
+export default (args: ViteConfig) => {
+  const generateScopedName =
+    args.mode === "production" ? "[hash:base64:6]" : "[local]__[hash:base64:3]";
+
+  return defineConfig({
+    base: APP_BASE_URL,
+    build: {
+      outDir: path.resolve(__dirname, "../../dist"),
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@shared/styles/_prepend.scss";`,
-        api: "modern-compiler",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@app": path.resolve(__dirname, "./src/app"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@widgets": path.resolve(__dirname, "./src/widgets"),
+        "@features": path.resolve(__dirname, "./src/features"),
+        "@entities": path.resolve(__dirname, "./src/entities"),
+        "@shared": path.resolve(__dirname, "./src/shared"),
       },
     },
-  },
-});
+    css: {
+      devSourcemap: true,
+      postcss: {
+        plugins: [autoprefixer],
+      },
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+          additionalData: `@import "@shared/styles/_prepend.scss";`,
+        },
+      },
+      modules: {
+        localsConvention: "camelCase",
+        generateScopedName,
+      },
+    },
+  });
+};
